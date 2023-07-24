5a842c345dfeafb0e34f0554a506cb2978bc6fe2
2147006 <2147006@LTIN430051.cts.com>
2023-07-24 20:11:38 +0530

    My ninth commit

---
+++ spring-security-demo-03-custom-login-form/src/main/java/com/varsha/springsecurity/demo/config/SecurityWebApplicationInitialzer.java
@@ -0,0 +1,7 @@
+package com.varsha.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitialzer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ spring-security-demo-03-custom-login-form/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+
+<title>Amigo Company Home Page</title>
+
+</head>
+
+<body>
+	<h2>Amigo Company Home Page - Woww!!!</h2>
+	<hr>
+	
+	
+	Welcome to the Amigo company home page!
+	
+</body>
+
+</html>
